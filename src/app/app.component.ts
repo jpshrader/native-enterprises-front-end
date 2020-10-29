99468048b56dfd972bf2e9c7e3af1193c3b06c6b
jpshrader <46694288+jpshrader@users.noreply.github.com>
2020-10-29 04:04:14 -0400

    Remove jasmine and replace with jest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'native-enterprises';
+  title = 'native-enterprises-front-end';
 }

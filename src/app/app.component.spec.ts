e0f8d1d41603a03823e429fa5d1c371e44cbbc45
jpshrader <46694288+jpshrader@users.noreply.github.com>
2021-02-02 15:27:47 -0500

    project name change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'native-enterprises-front-end'`, () => {
+  it(`should have as title 'frens-front-end'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('native-enterprises-front-end');
+    expect(app.title).toEqual('frens-front-end');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('native-enterprises-front-end app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('frens-front-end app is running!');
   });
 });
